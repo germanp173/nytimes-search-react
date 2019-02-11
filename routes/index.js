18fd8658b5de5b11d9e59396577f252048f63622
German Pineda <germanp173@gmail.com>
2019-02-10 18:14:30 -0800

    Add root route

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.use('/api', apiRoutes);
 
 // Send React app if no API routers are hit.
 // The client build directory is created after the react app is built.
-router.use(function(req, res){
+router.use('/', function(req, res){
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
